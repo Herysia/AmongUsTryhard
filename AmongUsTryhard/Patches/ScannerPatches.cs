13ac77df05fc67de487033bd5e853be51044bcc2
Herysia <totogirerd@gmail.com>
2021-01-29 16:00:43 +0100

    Disable medbay "Waiting for scan", everyone can scan at the same time

---
+++ AmongUsTryhard/Patches/ScannerPatches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using PowerTools;
+using UnityEngine;
+
+namespace AmongUsTryhard.Patches
+{
+    internal class ScannerPatches
+    {
+        [HarmonyPatch(typeof(MedScanMinigame), nameof(MedScanMinigame.FixedUpdate))]
+        public static class MedScanMinigame_FixedUpdate
+        {
+            public static void Prefix(MedScanMinigame __instance)
+            {
+                if (__instance.MyNormTask.IsComplete)
+                {
+                    return;
+                }
+                byte playerId = PlayerControl.LocalPlayer.PlayerId;
+                if (__instance.medscan.CurrentUser != playerId)
+                {
+                    __instance.medscan.CurrentUser = playerId;
+                }
+            }
+        }
+    }
+}
